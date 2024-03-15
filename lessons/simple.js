21d4624bd238eba65eba8df4630e12e796a32e47
Lev <levdevuk@gmail.com>
2024-03-15 17:06:27 +0000

    Throw an Error with a Simple Test in JavaScript | COMPLETE

--- lessons/simple.js
+++ lessons/simple.js
@@ -1,9 +1,14 @@
-const sum = (a, b) => a - b
+const sum = (a, b) => a + b
 const subtract = (a, b) => a - b
 
-const result = sum(3, 7)
-const expected = 10
+let result = sum(3, 7)
+let expected = 10
 if (result !== expected) {
   throw new Error(`${result} is not equal to ${expected}`)
 }
 
+result = subtract(7, 3)
+expected = 4
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`)
+}
